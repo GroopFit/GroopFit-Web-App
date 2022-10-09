030a05f02a13443f3d39c6917f8a1bb6c948540b
Sheldon Jackson <Sheldon.ej@gmail.com>
2022-10-09 15:38:17 -0400

    Starting Groop Page

---
+++ client/src/components/pages/DashboardGroops/GroopLeaderboard.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { connect } from 'react-redux';
+import * as AiIcons from "react-icons/ai";
+import * as FA from "react-icons/fa";
+import GroopLeaderboardElement from "./GroopLeaderboardElement"
+
+
+class GroopLeaderboard extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { 
+          
+        };
+    }
+
+
+    componentDidMount(){
+
+    } 
+
+    
+
+    render() {
+        return  (
+        <div className="groopfit-container" >
+            <div className="leaderboard-header" >
+                <div className="leaderboard-header-l">
+                <div className="leaderboard-l-box leaderboard-title">Leaderboard</div>
+                <div className="leaderboard-l-box flex flow-row"> 
+                    <div className="leaderboard-picker"> Today </div>
+                    <div className="leaderboard-picker"> Week </div>
+                    <div className="leaderboard-picker"> Month </div>
+                    <div className={`leaderboard-picker icon-bg-${"blue"} primary-text`} > All Time </div>
+                </div>
+                </div>
+                <div className="leaderboard-header-r">
+                <div className={`leaderboard-activity-icon icon-bg-${"green"} flex center`} >
+                    <FA.FaRunning color={'#fff'} fontSize={'2.0rem'} />
+                </div>
+                </div>
+            </div>
+            <div className="dashboard-horizontal-box-line"></div>
+            <div className="leaderboard-scroll flex flow-col">
+                <GroopLeaderboardElement picture={this.props.userData.userPicture} fullName={this.props.userData.fullName} 
+                                         ranking={1} stagnant={true} positionChangeAmount={0} value={1245} unit={"miles"} />
+
+                <GroopLeaderboardElement picture={this.props.userData.userPicture} fullName={this.props.userData.fullName} 
+                                         ranking={2} increase={true} positionChangeAmount={1} value={1076} unit={"miles"} />
+                <GroopLeaderboardElement picture={this.props.userData.userPicture} fullName={this.props.userData.fullName} 
+                                         ranking={3} decrease={true} positionChangeAmount={1} value={1054} unit={"miles"} />
+                <GroopLeaderboardElement picture={this.props.userData.userPicture} fullName={this.props.userData.fullName} 
+                                         ranking={4} increase={true} positionChangeAmount={2} value={1032} unit={"miles"} />
+                <GroopLeaderboardElement picture={this.props.userData.userPicture} fullName={this.props.userData.fullName} 
+                                         ranking={5} stagnant={true} positionChangeAmount={0} value={987} unit={"miles"} />
+                <GroopLeaderboardElement picture={this.props.userData.userPicture} fullName={this.props.userData.fullName} 
+                                         ranking={6} decrease={true} positionChangeAmount={1} value={891} unit={"miles"} />
+            </div>
+            <div className="dashboard-horizontal-box-line"></div>
+            <div className="leaderboard-footer">
+            <div className="leaderboard-footer-content" >
+                <div className="leaderboard-footer-content-box">
+                <div>1</div>
+                <div>Ranking</div>
+                </div>
+            </div>
+            <div className="leaderboard-footer-content" >
+                <div className="leaderboard-footer-mid-content" >
+                <img alt="" src={this.props.userData.userPicture} />
+                <div className="leaderboard-footer-mid-text" > You </div>
+                </div>
+            </div>
+            <div className="leaderboard-footer-content" >
+            <div className="leaderboard-footer-content-box">
+                <div>1,456</div>
+                <div>Total Miles</div>
+                </div>
+            </div>
+            </div>
+        </div>
+    )}
+}
+
+const MapStateToProps = (state) => ({ 
+    userData: state.userData.info,
+    groopData: null,
+  });
+export default connect(MapStateToProps)(GroopLeaderboard)
--- client/src/components/pages/DashboardGroops/index.js
+++ client/src/components/pages/DashboardGroops/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 import { connect } from 'react-redux';
+import GroopLeaderboard from "./GroopLeaderboard"
+
+//AiOutlineCaretUp
+//AiOutlineLine
+//AiOutlineCaretDown
 
 class DashboardGroop extends React.Component {
 
@@ -12,31 +17,45 @@ class DashboardGroop extends React.Component {
 
 
     componentDidMount(){
+      console.log(this.props.userData)
 
     } 
     
 
   render() {
     return  <div className="groop-grid">
-                <div className="groop-grid-b1" >
-                  <div className="groopfit-container" />
+                <div className="groop-grid-b1 flex flow-col" >
+                  <div className="groop-dash-title-box">
+                    <div className="groop-dash-logo-box  flex center" />
+                    <div className="groop-dash-title" >
+                      RPI Fitness Club
+                      <div className="groop-dash-title-line " />
+                    </div>
+                  </div>
+                  <div className="groop-dash-text-wrap">
+                    <div className="groop-dash-text">
+                      <b style={{fontSize: '16px', color: '#fff'}}>Group Type:</b> Competetive
+                    </div>
+                    <div className="groop-dash-text">
+                      <b style={{fontSize: '16px', color: '#fff'}}>Description:</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </div>
+                  </div>
                 </div>
                 <div className="groop-grid-b2" >
                   <div className="groopfit-container" />
+                  <div className="groopfit-container" />
                 </div>
                 <div className="groop-grid-b3" >
-                  <div className="groopfit-container" />
+                  <GroopLeaderboard />
                 </div>
                 <div className="groop-grid-b4" >
-                  <div className="groopfit-container" />
+                  <div className="groopfit-container" >
+                    </div>
                 </div>
                 <div className="groop-grid-b5 " >
                   <div className="groopfit-container" />
                 </div>
                 <div className="groop-grid-b6" >
-                  <div className="groopfit-container" />
-                </div>
-                <div className="groop-grid-b7" >
                   <div className="groopfit-container" />
                 </div>
             </div>
