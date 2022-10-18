9cc25f29f12d5c318762db31db2b96af34d24fcc
Sheldon Jackson <Sheldon.ej@gmail.com>
2022-10-18 15:52:32 -0400

    Overview Page Visual Updates

--- client/src/components/pages/DashboardHome/index.js
+++ client/src/components/pages/DashboardHome/index.js
@@ -2,12 +2,74 @@ import React from "react";
 import { connect } from 'react-redux';
 import * as TI from "react-icons/ti";
 import * as IO from "react-icons/io";
+import * as IO5 from "react-icons/io5";
 import * as FA from "react-icons/fa";
+import * as HI from "react-icons/hi";
 import ReactApexChart from 'react-apexcharts'
 import UserInfoCard from './UserInfoCard'
 import SmallGraphCard from './SmallGraphCard'
+import SmallButtonCard from './SmallButtonCard'
 
-
+let series2 =  [
+  {
+    name: 'Progress',
+    data: [
+      {
+        x: 'Running',
+        y: 8,
+        goals: [
+          {
+            name: 'Average',
+            value: 10,
+            strokeHeight: 5,
+            strokeLineCap: 'round',
+            strokeColor: '#775DD0'
+          }
+        ]
+      },
+      {
+        x: 'Walking',
+        y: 12,
+        goals: [
+          {
+            name: 'Average',
+            value: 8,
+            strokeHeight: 5,
+            strokeLineCap: 'round',
+            strokeColor: '#775DD0'
+          }
+        ]
+      },
+      {
+        x: 'Swimming',
+        y: 20,
+        goals: [
+          {
+            name: 'Average',
+            value: 15,
+            strokeHeight: 13,
+            strokeWidth: 0,
+            strokeLineCap: 'round',
+            strokeColor: '#775DD0'
+          }
+        ]
+      },
+      {
+        x: 'Biking',
+        y: 5,
+        goals: [
+          {
+            name: 'Average',
+            value: 3,
+            strokeHeight: 3,
+            strokeDashArray: 3,
+            strokeColor: '#775DD0'
+          }
+        ]
+      }
+    ]
+  }
+]
 
 const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
@@ -19,6 +81,69 @@ class DashboardHome extends React.Component {
     constructor(props) {
         super(props);
         this.state = { 
+
+          options2: {
+            chart: {
+              type: 'bar',
+              foreColor: '#888888',
+              fontFamily: 'Montserrat, Arial, sans-serif',
+              dropShadow: {
+                enabled: true,
+                top: 1,
+                left: 1,
+                blur: 2,
+                opacity: 0.2,
+              }
+            },
+            title: {
+              text: "Your Weekly Statistics",
+              style: {
+                color: "#fff",
+                fontSize: '20px',
+                fontWeight: "bold",
+                fontFamily: 'Montserrat, Arial, sans-serif',
+              },
+            },
+            fill: {
+          
+            },
+            plotOptions: {
+              bar: {
+                columnWidth: '60%'
+              }
+            },
+            colors: ['#00E396'],
+            dataLabels: {
+              enabled: false
+            },
+            legend: {
+              show: true,
+              position: 'bottom',
+              fontSize: '16px',
+              color: "#fff",
+              fontSize: '18px',
+              fontFamily: 'Montserrat, Arial, sans-serif',
+              showForSingleSeries: true,
+              customLegendItems: ['&nbsp;' + 'This Week', '&nbsp;' + 'Average'],
+              markers: {
+                fillColors: ['#00E396', '#775DD0']
+              }
+            },
+            xaxis: {
+              labels: {
+                style: {
+                  fontSize: '16px',
+                }
+              }
+            },
+            yaxis: {
+              labels: {
+                style: {
+                  fontSize: '16px',
+                }
+              }
+            }
+          },
 
           options: {
             chart: {
@@ -32,7 +157,7 @@ class DashboardHome extends React.Component {
                   opacity: 0.2,
                 }
             },
-            labels: ["Value 1", "Value 2", "Value 3", "Value 4", "Value 5"],
+            labels: [" Walking", " Running", " Swimming", " Biking", " Other"],
             dataLabels: {
               enabled: false,
 
@@ -41,6 +166,8 @@ class DashboardHome extends React.Component {
               show: true,
               position: 'left',
               color: "#fff",
+              fontSize: '16px',
+              fontFamily: 'Montserrat, Arial, sans-serif',
             },
             fill: {
               type: 'gradient',
@@ -57,14 +184,15 @@ class DashboardHome extends React.Component {
                     color: '#fff',
                     total: {
                       show: true,
+                      label: 'Total\nHours',
                       color: '#fff',
-                      fontSize: '22px',
+                      fontSize: '18px',
                       fontFamily: 'Montserrat, Arial, sans-serif',
                       fontWeight: 'bold',
                     },
                     value: {
                       color: '#fff',
-                      fontSize: '20px',
+                      fontSize: '18px',
                       fontFamily: 'Montserrat, Arial, sans-serif',
                     },
             
@@ -107,30 +235,38 @@ class DashboardHome extends React.Component {
                   <div className="overview-time-breakdown-card-top" > Workout Hours Breakdown </div>
                   <div className="overview-time-breakdown-card-mid flex center">
                     <div className="overview-time-breakdown-card-chart flex center">
-                      <ReactApexChart options={this.state.options} series={[44, 55, 41, 17, 15]} type="donut" height={275} width={375} />
+                      <ReactApexChart options={this.state.options} series={[44, 55, 41, 17, 15]} type="donut" height={300} width={400} />
                     </div>
                     <div className="overview-time-breakdown-card-mid-content">
-                      <div className="tiny-text flex flow-row baseline "><div className="dashboard-card-percentage">8%</div> {" " + "Increase"}  calculated over the last 30 days</div>
+                      <div className="tiny-text flex flow-row baseline "><div className="dashboard-card-percentage">8%</div> {" " + "Increase"} this month compared to last month</div>
                     </div>
                   </div>
                   <div className="overview-time-breakdown-card-bottom " >
-                    <button className="flex center"> More Details </button>
+                    <button className="flex center" onClick={() => alert("This Functionality is Incomplete")}> More Details </button>
 
                   </div>
                 </div>
               </div>  
               <div className="overview-container-2 dashboard-container" >
-                <div className="new-blank-card two-by-two" >
+                <div className="new-blank-card  two-by-two" >
                   <SmallGraphCard color={"blue"} activity={"running"} data={[ 1,2,0,4,5,2,6 ]} units={"Miles"} />
-                  <SmallGraphCard color={"pink"} activity={"walking"} data={[ 1,2,3,0,0,2,4 ]} units={"Miles"} />
-                  <SmallGraphCard color={"purple"} activity={"biking"} data={[ 0,8,4,0,8,7,2 ]} units={"Miles"} />
-                  <SmallGraphCard color={"teal"} activity={"swimming"} data={[ 20,25,28,22,17,27,25 ]} units={"Meters"} />
+                  <SmallButtonCard title="Join Groop" color="purple" subtitle={`In ${1} out of ${4}`}  buttonText="Join Groop">
+                    <HI.HiUserGroup  color={'#fff'} fontSize={'1.80rem'} />
+                  </SmallButtonCard>
+                  <SmallButtonCard title="Add Goals" color="pink" subtitle={`Test Yourself`}  buttonText="Add Goals">
+                    <IO5.IoRibbon color={'#fff'} fontSize={'1.80rem'} />
+                  </SmallButtonCard>
+                  <SmallGraphCard color={"teal"} activity={"walking"} data={[ 30,25,28,22,17,27,25 ]} units={"Meters"} />
                 
                 </div>
               </div>  
               <div className="overview-container-3  dashboard-container" >
                  {/* THESE ARE THE SAME!!!!!! */}
-                <div className="new-dashboard-card flex center" > 6 </div>
+                <div className="new-dashboard-card goal-bar-chart-card flex center" >
+                  <div className="goal-bar-chart">
+                    <ReactApexChart options={this.state.options2} series={series2} type="bar" height={"100%"} width={"100%"} />
+                  </div>
+                </div>
               </div>  
             </div>
 {/* ========================================================================================================= */}
@@ -142,7 +278,11 @@ class DashboardHome extends React.Component {
         <div className="dashboard-flex-grid-wrapper margin-top-none">
           <div className="overview-container-4  dashboard-container" >
             {/* THESE ARE THE SAME!!!!!! */}
-            <div className="new-dashboard-card flex center" > 6 </div>
+            <div className="new-dashboard-card goal-bar-chart-card flex center" >
+              <div className="goal-bar-chart">
+                <ReactApexChart options={this.state.options2} series={series2} type="bar" height={"100%"} width={"100%"} />
+              </div>
+            </div>
           </div> 
           <div className="dashboard-half-container overview-container-5 dashboard-container" >
             <div className="new-dashboard-card flex center" > 8 </div>
