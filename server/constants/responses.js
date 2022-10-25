87a140884ed150316953ae60c37ba4903ec60c4e
Miles <mgednie@gmail.com>
2022-10-25 17:20:02 -0400

    create constants

---
+++ server/constants/responses.js
@@ -0,0 +1,16 @@
+const Ok = 200;
+const Bad_request = 400;
+const Unauthorized = 401;
+const Forbidden = 403;
+const Not_found = 404;
+const Not_Allowed = 405;
+const Rate_limit = 429;
+const Internal_server_error = 500;
+const Bad_gateway = 502;
+const Unavailable = 503;
+const Timeout = 504;
+const HTTP_version_not_supported = 505;
+
+export {Ok, Bad_request, Unauthorized, Forbidden, Not_found, Not_Allowed, Rate_limit, 
+    Internal_server_error, Bad_gateway, Unavailable, Timeout, HTTP_version_not_supported}
+
