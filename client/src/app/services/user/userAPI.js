0167456de4f872e3ea4cfd0601439e64323db167
pathfold <52586263+pathfold@users.noreply.github.com>
2022-10-31 00:10:08 -0400

    added headers to axios fetch

--- client/src/app/services/user/userAPI.js
+++ client/src/app/services/user/userAPI.js
@@ -25,56 +25,65 @@ export const getUserDataAsync = createAsyncThunk(
     }  
 );
 
+// export const pingpongAsync = createAsyncThunk(
+//     'user/pingpong/pingpongAsync',
+//     async (payload, thunkAPI) => {
+//         console.log("Starting to send data: ", payload)
+//         let fetchRoute = `http://localhost:8000/pingpong`;
+//         try {
+//             const serverResponse = await fetch(fetchRoute, {
+//                 method: 'POST',
+//                 mode: 'cors',
+//                 headers: { 
+//                      'Content-Type': 'application/json',
+//                      'Authorization':  `Bearer ${store.getState().userData.auth.accessToken}`  
+//                  },
+//                 credentials: 'include',
+//                 body: JSON.stringify(payload)
+//             });
+//             // console.log("serverResponse " + serverResponse);
+//             if( !serverResponse.ok ){
+//                 if( serverResponse.status === 403 ){
+//                     await thunkAPI.dispatch(authAPI.refreshAccessTokenAsync())
+//                     /*--------------------------------------------------------------------/
+//                     /  Task: Limit the recursion to a max of 5 times using the variable   /
+//                     /  state.userData.info.refreshTokenRequestCount to do this.           /
+//                     /--------------------------------------------------------------------*/
+//                     await thunkAPI.dispatch(pingpongAsync(payload)).payload
+//                     return { err: 404 }
+//                 } 
+//                 throw new Error(`${serverResponse.status} ${serverResponse.statusText}`) 
+//             } 
+//             return await serverResponse.json()
+//         } catch (err) {
+//             console.log("TESTING")
+//         }
+//     }  
+// );
+
 export const pingpongAsync = createAsyncThunk(
     'user/pingpong/pingpongAsync',
     async (payload, thunkAPI) => {
         console.log("Starting to send data: ", payload)
         let fetchRoute = `http://localhost:8000/pingpong`;
-        try {
-            const serverResponse = await fetch(fetchRoute, {
-                method: 'POST',
-                mode: 'cors',
-                headers: { 
-                     'Content-Type': 'application/json',
-                     'Authorization':  `Bearer ${store.getState().userData.auth.accessToken}`  
-                 },
-                credentials: 'include',
-                body: JSON.stringify(payload)
-            });
-            // console.log("serverResponse " + serverResponse);
-            if( !serverResponse.ok ){
-                if( serverResponse.status === 403 ){
-                    await thunkAPI.dispatch(authAPI.refreshAccessTokenAsync())
-                    /*--------------------------------------------------------------------/
-                    /  Task: Limit the recursion to a max of 5 times using the variable   /
-                    /  state.userData.info.refreshTokenRequestCount to do this.           /
-                    /--------------------------------------------------------------------*/
-                    await thunkAPI.dispatch(pingpongAsync(payload)).payload
-                    return { err: 404 }
-                } 
-                throw new Error(`${serverResponse.status} ${serverResponse.statusText}`) 
-            } 
-            return await serverResponse.json()
-        } catch (err) {
-            console.log("TESTING")
-        }
-    }  
-);
 
-export const pingpongAsyncNew = createAsyncThunk(
-    'user/pingpong/pingpongAsync',
-    async (payload, thunkAPI) => {
-        console.log("Starting to send data: ", payload)
-        let fetchRoute = `http://localhost:8000/pingpong`;
+        let config = {
+            headers: { 
+                'Content-Type': 'application/json',
+                'Authorization':  `Bearer ${store.getState().userData.auth.accessToken}`  
+            }
+        }
         
-        axios.post(fetchRoute, payload).then((response) => {
+        axios.post(fetchRoute, payload, config)
+        .then((response) => {
             console.log(response.data);
-        }).catch(async error => {
+        })
+        .catch(async error => {
             if (error.response.status === 403) {
                 await thunkAPI.dispatch(authAPI.refreshAccessTokenAsync());
                 console.log(store.getState().userData.info.refreshTokenRequestCount)
 
-                if (store.getState().userData.info.refreshTokenRequestCount <= 5) {
+                if (0 <= store.getState().userData.info.refreshTokenRequestCount) {
                     await thunkAPI.dispatch(pingpongAsync(payload)).payload;
                 }
                 return { err: 404 };
