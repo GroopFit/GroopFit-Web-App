535c2ff95425abaef423fdcac306dcefce139187
Miles <mgednie@gmail.com>
2022-11-01 17:19:29 -0400

    start skeleton of class

---
+++ server/classes/Activity.js
@@ -0,0 +1,14 @@
+const DatabaseManager = require('../database/DatabaseManager')
+
+class Activity{
+    //need to pass in email so we can query for the right User
+    constructor(type, name, distance, blah, blah){
+        //fill in fields with passed in stuff now
+        return 
+    }
+
+    //functions to update activity
+
+    //delete activity
+
+}
