ebaf9f2cef5d8ca10b36e0e611ceeca4322584ea
Kerui Wu <99207376+Yujjio@users.noreply.github.com>
2022-11-02 14:36:28 -0400

    deleted grid_box & added flex_box

--- client/src/components/pages/DashboardGroops/index.js
+++ client/src/components/pages/DashboardGroops/index.js
@@ -31,39 +31,45 @@ class DashboardGroop extends React.Component {
     
 
   render() {
-    return  <div className="groop-grid">
-                <div className="groop-grid-b1 flex flow-col" >
-                  <div className="groop-dash-title-box">
-                    <div className="groop-dash-logo-box  flex center" />
-                    <div className="groop-dash-title" >
-                      RPI Fitness Club
-                      <div className="groop-dash-title-line " />
+    return  <div className="flex flex_check_total">
+                <div className="flex flex_check1 flex_check">
+                  <div className="groop-grid-b1 flex flow-col" >
+                    <div className="groop-dash-title-box">
+                      <div className="groop-dash-logo-box  flex center" />
+                      <div className="groop-dash-title" >
+                        RPI Fitness Club
+                        <div className="groop-dash-title-line " />
+                      </div>
+                    </div>
+                    <div className="groop-dash-text-wrap">
+                      <div className="groop-dash-text">
+                        <b style={{fontSize: '16px', color: '#fff'}}>Groop Type: </b>Competetive
+                      </div>
+                      <div className="groop-dash-text">
+                        <b style={{fontSize: '16px', color: '#fff'}}>Description: </b> 
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna 
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                      </div>
                     </div>
                   </div>
-                  <div className="groop-dash-text-wrap">
-                    <div className="groop-dash-text">
-                      <b style={{fontSize: '16px', color: '#fff'}}>Groop Type: </b>Competetive
-                    </div>
-                    <div className="groop-dash-text">
-                      <b style={{fontSize: '16px', color: '#fff'}}>Description: </b> 
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                      sed do eiusmod tempor incididunt ut labore et dolore magna 
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </div>
-                  </div>
+                  <div className="groop-grid-b4" ><GroopRecentActivity /></div>
                 </div>
-                <div className="groop-grid-b2" >
-                  <div className="groopfit-container flex center icon-bg-blue" >
-                    <GoIcons.GoGraph color={'#fff'}  fontSize={'5.00rem'} />
+                <div className="flex flex_check2 flex_check">
+                  <div className="groop-grid-b2" >
+                    <div className="groopfit-container flex center icon-bg-blue" >
+                      <GoIcons.GoGraph color={'#fff'}  fontSize={'5.00rem'} />
+                    </div>
                   </div>
+                  <div className="groop-grid-b5" ><GroopMemberList /></div>
                 </div>
-                <div className="groop-grid-b3" ><GroopLeaderboard /></div>
-                <div className="groop-grid-b4" ><GroopRecentActivity /></div>
-                <div className="groop-grid-b5" ><GroopMemberList /></div>
-                <div className="groop-grid-b6" >
-                  <div className="groopfit-container flex center icon-bg-pink" >
-                    <GoIcons.GoGraph color={'#fff'}  fontSize={'5.00rem'} />
+                <div className="flex flex_check3 flex_check">
+                  <div className="groop-grid-b3" ><GroopLeaderboard /></div>
+                  <div className="groop-grid-b6" >
+                    <div className="groopfit-container flex center icon-bg-pink" >
+                      <GoIcons.GoGraph color={'#fff'}  fontSize={'5.00rem'} />
+                    </div>
                   </div>
                 </div>
             </div>
