a3b82b420c3c4cf3c0b1db44f57561189507a24a
pathfold <52586263+pathfold@users.noreply.github.com>
2022-11-06 15:29:16 -0500

    axios request/response interceptors added

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import store from './app/store';
 import AppRouter from './AppRouter';
 import './index.css';
+import axios from 'axios';
 //import dotenv from  'dotenv'
 //import path from 'path'
 
@@ -11,6 +12,28 @@ import './index.css';
 //dotenv.config({ path: `../.env` })
 
 //import reportWebVitals from './reportWebVitals';
+
+// request interceptor
+axios.interceptors.request.use(
+  function (successfulReq) {
+    return successfulReq;
+  },
+  function (err) {
+    return Promise.reject(err);
+  }
+);
+
+// response interceptor
+axios.interceptors.response.use(
+  undefined,
+  function retry(err) {
+    if (err.response.status === 403) {
+      console.log("error " + err);
+    }
+    return Promise.reject(err);
+  }
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
