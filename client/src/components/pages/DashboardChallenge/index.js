c0f7087b62b916b624eb47e8327db397e71f260a
Kerui Wu <99207376+Yujjio@users.noreply.github.com>
2022-11-28 21:24:06 -0500

    challange layout

--- client/src/components/pages/DashboardChallenge/index.js
+++ client/src/components/pages/DashboardChallenge/index.js
@@ -21,10 +21,30 @@ class DashboardChallenge extends React.Component {
     
 
   render() {
-    return  <div className="dash-inner-wrapper">
-                <div className="full-wh flex center" >
+    return  <div className="dash-inner-wrapper dashboard-page-scroll-wrapper">
+                {/* <div className="full-wh flex center" >
                     <button onClick={() => {this.pingPongButtonPress()}} style={{ height: '30px', width: '100px' }}> Press Me </button>
+                </div> */}
+                <div className="dashboard-full-containers height-300 dashboard-card">
+                  <div className="flex center">PK bar</div>
                 </div>
+                <div className="flex flex_row height_100">
+                  <div className="dashboard-two-third-container flex flex_col height_100">
+                    <div className="dashboard-card height_20">
+                      <div className="flex center">Invite team for competing</div>
+                    </div>
+                    <div className="dashboard-card height_15">
+                      <div className="flex center">Goal category</div>
+                    </div>
+                    <div className="dashboard-card height_20">
+                    <div className="flex center">Set Group Goal</div>
+                    </div>
+                  </div>
+                  <div className="dashboard-one-third-container dashboard-card height_100">
+                    <div className="flex center">Group Ranking</div>
+                  </div>
+                </div>
+                
             </div>
   }
 }
