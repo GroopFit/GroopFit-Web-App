744bfab9f2ac813951dac78e422e447a83208cdb
SpadeZap <spadezap@gmail.com>
2022-12-09 16:33:03 -0500

    Added Functions to Functions.js
    I added a function to check if there is a valid string, to make the first letter of a word uppercase, and one to make the all words in a string uppercase.

--- client/src/_Functions.js
+++ client/src/_Functions.js
@@ -4,7 +4,27 @@
 //TASK: Make a function that Capitalizes the first letter in every word that occurs "dog cat" -> "Dog Cat"
 //TASK: Make a function to Concatenate Jsons
 
+stringComparison = (str1, str2) => {
+    if(typeof(str1) !== 'string' || typeof(str2) !== 'string'){
+        exit(-1);
+    }
+    return str1.toLowerCase() == str2.toLowerCase();
+}
 
+capString = (str) => {
+    str = str.charAt(0).toUpperCase() + str.slice(1);
+    return str;
+}
+
+capStrings = (str_array) => {
+    let str_array_cop;
+    str_array.split(" ");
+    for (let i = 0; i < str_array.size(); i++) {
+        str_array_cop += capString(str_array[i]);
+    }
+    return str_array_cop;
+    
+}
 
 
 getActivityColor = (activityName, activityColors) => {
